d7dbf07995846b937f022858c3ae46235817dde8
Arul Kumar <arulkumar202@gmail.com>
2022-09-27 16:29:11 +0530

    Initial commit

---
+++ src/Sms.java
@@ -0,0 +1,6 @@
+public class massage {
+    String play = "Enter 1 to play the game or 0 to exit the game :";
+    String given = "This is you're number ";
+    String numSms = "Use these numbers only: ";
+
+}
